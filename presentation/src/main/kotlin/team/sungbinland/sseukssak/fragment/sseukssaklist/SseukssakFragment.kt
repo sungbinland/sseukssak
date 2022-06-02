b0586a8423c47cabc1be69fa2c8fc37a5d9603c0
ChangXXX <eqqt97@naver.com>
2022-06-03 08:28:21 +0900

    feat: add sseukssak list fragment

---
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/fragment/sseukssaklist/SseukssakFragment.kt
@@ -0,0 +1,27 @@
+/*
+ * SseukSsak © 2022 Team Sungbinland. all rights reserved.
+ * SseukSsak license is under the MIT.
+ *
+ * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
+ */
+
+/*
+ * SseukSsak © 2022 Team Sungbinland. all rights reserved.
+ * SseukSsak license is under the MIT.
+ *
+ * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
+ */
+
+package team.sungbinland.sseukssak.fragment.sseukssaklist
+
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import team.sungbinland.sseukssak.R
+import team.sungbinland.sseukssak.base.BaseFragment
+import team.sungbinland.sseukssak.databinding.FragmentSseukssakBinding
+
+@AndroidEntryPoint
+class SseukssakFragment : BaseFragment<FragmentSseukssakBinding>(R.layout.fragment_sseukssak) {
+    val vm: SseukssakViewModel by viewModels()
+
+}
