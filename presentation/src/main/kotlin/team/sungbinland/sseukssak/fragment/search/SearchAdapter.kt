aaff451224c7614e252116548fb78791694db620
ckrudals <2004ckck@naver.com>
2022-06-08 16:48:51 +0900

    feat : Add SearchAdapter

---
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/fragment/search/SearchAdapter.kt
@@ -0,0 +1,60 @@
+/*
+ * SseukSsak © 2022 Team Sungbinland. all rights reserved.
+ * SseukSsak license is under the MIT.
+ *
+ * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
+ */
+
+package team.sungbinland.sseukssak.fragment.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import team.sungbinland.sseukssak.R
+import team.sungbinland.sseukssak.data.search.db.SearchEntity
+import team.sungbinland.sseukssak.databinding.ItemSearchSseukssakBinding
+
+class SearchAdapter(
+) : ListAdapter<SearchEntity, SearchAdapter.SearchViewHolder>(
+    diffUtil
+) {
+
+    class SearchViewHolder(private val binding: ItemSearchSseukssakBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(entity: SearchEntity) {
+
+            binding.entity = entity
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val binding: ItemSearchSseukssakBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_search_sseukssak,
+            parent,
+            false
+        )
+
+        return SearchViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<SearchEntity>() {
+            override fun areContentsTheSame(oldItem: SearchEntity, newItem: SearchEntity) =
+                oldItem == newItem
+
+            override fun areItemsTheSame(oldItem: SearchEntity, newItem: SearchEntity) =
+                oldItem.id == newItem.id
+        }
+    }
+}
