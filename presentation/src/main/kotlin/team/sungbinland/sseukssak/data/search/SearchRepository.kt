207891eb51ce6ae0258d972f2bf999adb3f8b372
ckrudals <2004ckck@naver.com>
2022-06-12 19:13:06 +0900

    docs: Delete duplicate documents

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/data/search/SearchRepository.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/data/search/SearchRepository.kt
@@ -5,12 +5,6 @@
  * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
  */
 
-/*
- * SseukSsak © 2022 Team Sungbinland. all rights reserved.
- * SseukSsak license is under the MIT.
- *
- * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
- */
 
 package team.sungbinland.sseukssak.data.search
 
