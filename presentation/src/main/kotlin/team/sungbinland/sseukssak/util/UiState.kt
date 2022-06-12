a360ca4b6731d6b5bb54bb39d1ea442b7c81eeb8
ckrudals <2004ckck@naver.com>
2022-06-12 19:24:33 +0900

    refactor : Update Throwable non null

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/util/UiState.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/util/UiState.kt
@@ -11,5 +11,5 @@ sealed class UiState<out T> {
     object Loading: UiState<Nothing>()
     object Uninitialized : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
-    data class Error(val error: Throwable?):UiState<Nothing>()
+    data class Error(val error: Throwable):UiState<Nothing>()
 }
