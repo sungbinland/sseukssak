992210f9054fcf5330374f314ff85429e6f2c720
ckrudals <2004ckck@naver.com>
2022-06-18 23:16:32 +0900

    feat : Add fireBaseModule

---
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/di/FirebaseModule.kt
@@ -0,0 +1,25 @@
+/*
+ * SseukSsak © 2022 Team Sungbinland. all rights reserved.
+ * SseukSsak license is under the MIT.
+ *
+ * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
+ */
+
+package team.sungbinland.sseukssak.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStore() = FirebaseFirestore.getInstance()
+
+}
