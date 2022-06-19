cf018cc465d8ffa05130f84c551c410511ee8692
ckrudals <2004ckck@naver.com>
2022-06-20 07:58:53 +0900

    refactor : Update text isEmpty

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/fragment/create/NewCreateSeeukSSakFragment.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/fragment/create/NewCreateSeeukSSakFragment.kt
@@ -22,52 +22,65 @@
 package team.sungbinland.sseukssak.fragment.create
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.jisungbin.logeukes.logeukes
 import team.sungbinland.sseukssak.R
 import team.sungbinland.sseukssak.base.BaseFragment
+import team.sungbinland.sseukssak.data.create.model.NewCreateSseukssak
 import team.sungbinland.sseukssak.databinding.FragmentNewCreateSseukssakBinding
 import team.sungbinland.sseukssak.util.extensions.UiState
 import team.sungbinland.sseukssak.util.extensions.toast
 
+@AndroidEntryPoint
 class NewCreateSeeukSSakFragment :
     BaseFragment<FragmentNewCreateSseukssakBinding>(R.layout.fragment_new_create_sseukssak) {
 
     private val viewModel: NewCreateSseukssakViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding.apply {
-            view = this@NewCreateSeeukSSakFragment
-            vm = viewModel
-        }
-
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            binding.view = this@NewCreateSeeukSSakFragment
+            vm = viewModel
+        }
         observeUiState()
     }
 
-    private fun observeUiState(){
-        lifecycleScope.launchWhenStarted {
-            viewModel.uiState.collect { uiState ->
-                when (uiState) {
-                    is UiState.Success<*> -> {
-                        // todo list 화면으로 이동
+    private fun observeUiState() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.uiState.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { uiState ->
+                    when (uiState) {
+                        is UiState.Success<*> -> {
+                            logeukes { "성공 : ${uiState.data}" }
+                            contentIsVisible()
+                            //todo list fragment 로 이동
+                        }
+                        is UiState.Error -> toast("쓱싹 생성 실패")
+                        else -> {
+                            logeukes { uiState }
+
+                        }
                     }
-                    is UiState.Error -> toast("쓱싹 생성 실패")
                 }
-            }
         }
     }
+
+    private fun contentIsVisible() {
+        binding.apply {
+            hashtagEditText.text = null
+            memoEditText.text = null
+            titleEditText.text = null
+            linkEditTxt.text = null
+        }
+    }
+
 
     fun newCreateSseukssak() {
         val memo = binding.memoEditText.text.toString()
@@ -75,26 +88,22 @@ class NewCreateSeeukSSakFragment :
         val link = binding.linkEditTxt.text.toString()
         val hashTag = hashTag(binding.hashtagEditText.text.toString())
 
+        logeukes { hashTag }
         when {
-            memo == "" -> toast("메모를 입력해 주세요.")
-            title == "" -> toast("제목를 입력해 주세요.")
-            link == "" -> toast("링크를 입력해 주세요.")
-            hashTag == emptyArray<String>() -> toast("hashTag를 입력해 주세요.")
+            memo.isEmpty() -> toast("메모를 입력해 주세요.")
+            title.isEmpty() -> toast("제목를 입력해 주세요.")
+            link.isEmpty() -> toast("링크를 입력해 주세요.")
+            hashTag.isEmpty() -> toast("hashTag를 입력해 주세요.")
             else -> {
-                val data =
-                    NewCreateSseukssakViewModel.NewCreateSseukssak(memo, title, link, hashTag)
+                val data = NewCreateSseukssak(memo, title, link, hashTag)
                 viewModel.newCreateSseukssak(data)
             }
         }
     }
 
-    private fun hashTag(hashTag: String): ArrayList<String> {
+    private fun hashTag(hashTag: String): List<String> {
 
-        return if (hashTag.contains(",")) {
-            hashTag.split(",") as ArrayList<String>
-        } else {
-            arrayListOf(hashTag)
-        }
+        return hashTag.split(",")
 
     }
 
