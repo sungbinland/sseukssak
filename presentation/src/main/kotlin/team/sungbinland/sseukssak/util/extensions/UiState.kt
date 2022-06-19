c189569bee7e70986963be6f19427f97521368cd
ckrudals <2004ckck@naver.com>
2022-06-20 07:59:42 +0900

    refactor : Update uistate

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/util/extensions/UiState.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/util/extensions/UiState.kt
@@ -7,8 +7,9 @@
 
 package team.sungbinland.sseukssak.util.extensions
 
-sealed class UiState {
-    object Loading: UiState()
-    data class Success<T>(val data: T): UiState()
-    data class Error(val error: Throwable?): UiState() 
+sealed class UiState<out T> {
+    object Loading: UiState<Nothing>()
+    object Uninitialized : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error(val error: Throwable):UiState<Nothing>()
 }
