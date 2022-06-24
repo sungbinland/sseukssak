e03b14f15d1216465cd124d7489ce38403dd27c5
ckrudals <2004ckck@naver.com>
2022-06-25 02:15:44 +0900

    refactor : Add id default value

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/data/search/db/SearchEntity.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/data/search/db/SearchEntity.kt
@@ -5,28 +5,14 @@
  * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
  */
 
-/*
- * SseukSsak © 2022 Team Sungbinland. all rights reserved.
- * SseukSsak license is under the MIT.
- *
- * Please see: https://github.com/sungbinland/sseukssak/blob/main/LICENSE.
- */
-
 package team.sungbinland.sseukssak.data.search.db
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import team.sungbinland.sseukssak.data.search.db.SearchEntity.Companion.SEARCH_TABLE
 
-@Entity(tableName = SEARCH_TABLE)
+@Entity(tableName = "search_table")
 data class SearchEntity(
     val searchText: String,
     @PrimaryKey(autoGenerate = true)
-    val id: Int
-) {
-
-
-    companion object {
-        const val SEARCH_TABLE = "search_table"
-    }
-}
+    val id: Int = 0
+)
