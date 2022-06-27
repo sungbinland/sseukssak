3145e1ddbd04bb5e72c7bef7685a03840951d4d0
ckrudals <2004ckck@naver.com>
2022-06-27 13:54:43 +0900

    refactor : Add kdoc and Update hashTag return value emptyList()

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/data/create/model/NewCreateSseukssak.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/data/create/model/NewCreateSseukssak.kt
@@ -7,9 +7,15 @@
 
 package team.sungbinland.sseukssak.data.create.model
 
+/**
+ * @param link : 링크
+ * @param title : 쓱싹 제목
+ * @param link : 메모
+ * @param hashTag : 해시태그
+ */
 data class NewCreateSseukssak(
     val link: String = "",
     val title: String = "",
     val memo: String = "",
-    val hashTag: List<String> = listOf()
+    val hashTag: List<String> = emptyList()
 )
