8c37427222947530f19a855d0cede0218138a995
mangbaam <pmb0836@gmail.com>
2022-07-01 01:19:11 +0900

    feat: add KDoc

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/activity/login/LoginRepository.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/activity/login/LoginRepository.kt
@@ -13,10 +13,20 @@ import team.sungbinland.sseukssak.util.extensions.set
 import javax.inject.Inject
 
 class LoginRepository @Inject constructor(private val preference: SharedPreferences) {
+    /**
+     * 유저 토큰 저장
+     *
+     * @param userId UserApiClient.instance.me.user.id 저장
+     * */
     fun saveUserId(userId: Long) {
         preference[USER_ID] = userId
     }
 
+    /**
+     * 로그인 상태 확인
+     *
+     * @return 유저 id가 존재하는 경우: true / 존재하지 않는 경우: false
+     * */
     fun checkLoggedIn(): Boolean {
         return preference[USER_ID, 0L] != 0L
     }
--- presentation/src/main/kotlin/team/sungbinland/sseukssak/activity/login/LoginViewModel.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/activity/login/LoginViewModel.kt
@@ -20,6 +20,11 @@ class LoginViewModel @Inject constructor(private val repository: LoginRepository
     private val _eventFlow = MutableSharedFlow<Event>()
     val eventFlow = _eventFlow.asSharedFlow()
 
+    /**
+     * 로그인 상태 확인
+     *
+     * @return 로그인 된 상태: LoginState.LoggedIn / 로그 아웃 상태: LoginState.LogOut
+     * */
     fun checkLoginState() =
         if (repository.checkLoggedIn()) {
             LoginState.LoggedIn
@@ -27,14 +32,27 @@ class LoginViewModel @Inject constructor(private val repository: LoginRepository
             LoginState.LogOut
         }
 
+    /**
+     * 유저 아이디 저장
+     *
+     * 유저 아이디가 저장되면 로그인 된 상태로 간주한다
+     *
+     * @param userId UserApiClient.instance.me.user.id
+     * */
     fun saveLoginState(userId: Long) {
         repository.saveUserId(userId)
     }
 
+    /**
+     * 카카오 로그인 UI 이벤트
+     * */
     fun kakaoLogin() {
         event(Event.KakaoLogin)
     }
 
+    /**
+     * 로그인 건너뛰기 UI 이벤트
+     * */
     fun skipLogin() {
         event(Event.SkipLogin)
     }
