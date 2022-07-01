3d9b0a697d47b54e5ea21bfe77f4e10853969d32
ckrudals <2004ckck@naver.com>
2022-07-01 21:04:31 +0900

    feat : Update kdoc

--- presentation/src/main/kotlin/team/sungbinland/sseukssak/data/create/repository/NewSseukssakRepository.kt
+++ presentation/src/main/kotlin/team/sungbinland/sseukssak/data/create/repository/NewSseukssakRepository.kt
@@ -10,13 +10,11 @@ package team.sungbinland.sseukssak.data.create.repository
 import team.sungbinland.sseukssak.data.create.model.NewCreateSseukssak
 import team.sungbinland.sseukssak.util.extensions.Result
 
-/**
- * 쓱싹 생성하기 위한 저장소 인터페이스.
- * @param newCreateSseukssak 쓱싹 생성하기 모델
- * @return 쓱싹 생성하기가 성공되면 성공" 메세지가 반환되고, 실패하면 Exception이  반환된다.
- */
-
 interface NewSseukssakRepository {
-
+    /**
+     * 쓱싹 생성하기
+     *  @param newCreateSseukssak 쓱싹 생성하기 모델
+     * @return 쓱싹 생성하기가 성공되면 성공" 메세지가 반환되고, 실패하면 Exception이  반환된다.
+     */
     suspend fun createSseukssak(newCreateSseukssak: NewCreateSseukssak): Result
 }
